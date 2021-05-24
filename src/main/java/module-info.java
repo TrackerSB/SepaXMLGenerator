89b46694722e079796d5bd8f8b96a829261ccba7
Stefan Huber <stefan.huber.niedling@outlook.com>
2021-05-24 15:16:58 +0200

    Gradle: Disable erroneous JavaDoc

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module bayern.steinbrecher.SepaXMLGenerator {
     exports bayern.steinbrecher.sepaxmlgenerator;
 
     requires java.compiler;
+    requires java.logging;
     requires java.xml;
     requires com.sun.tools.xjc;
 }
