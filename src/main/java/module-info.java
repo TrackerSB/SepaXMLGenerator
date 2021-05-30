84dd205537838ec0a1485cfd90fa19430a84ba1c
Stefan Huber <stefan.huber.niedling@outlook.com>
2021-05-30 09:18:31 +0200

    Gradle: Avoid including JAXB documentation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module bayern.steinbrecher.SepaXMLGenerator {
     exports bayern.steinbrecher.sepaxmlgenerator;
 
+    requires com.sun.tools.xjc;
+
     requires java.compiler;
     requires java.logging;
     requires java.xml;
-    requires com.sun.tools.xjc;
 }
