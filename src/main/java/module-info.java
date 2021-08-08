790e71c0f1073964363f53936a1d364eed701ea5
Stefan Huber <stefan.huber.niedling@outlook.com>
2021-08-08 16:49:39 +0200

    Gradle: Update RecordBuilder

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module bayern.steinbrecher.SepaXMLGenerator {
     exports bayern.steinbrecher.sepaxmlgenerator;
 
     requires com.sun.tools.xjc;
+    requires io.soabase.recordbuilder.core;
 
     requires java.compiler;
     requires java.logging;
